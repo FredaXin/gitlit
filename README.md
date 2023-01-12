5d596300fbe435f0a0622a0560bef5f2f33ec9e4
Freda Xin <freda.xin@gmail.com>
2023-01-11 22:15:58 -0500

    Add README, add git actions in sh script

---
+++ README.md
@@ -0,0 +1,6 @@
+###TODOs 
+- Create n empty commits 
+- Configure git repo for pushing to Main (Private Repo)
+- Push the changes (i.e. updates in the commit history) into Main
+- Create a repo for the template 
+    - Instantiate the template
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,18 @@ set -o xtrace
 
 lower=$1
 upper=$2
+user_name=$3
+user_email=$4
 echo "lower bound $lower; upper bound $upper"
 
 num_commits=$(awk "BEGIN{srand();print int(rand()*($upper-$lower))+$lower }")
+
+echo git config --global user.name $user_name
+echo git config --global user.email $user_email
+
+for c in $(seq 1 $num_commits)
+do
+    # Generate empty commits 
+    echo git commit --allow-empty -m "$c"
+done
+    
