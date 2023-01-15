751fc5d0857342d7dc295bc9cf126603caa00250
Freda Xin <freda.xin@gmail.com>
2023-01-15 18:16:03 -0500

    Push to main

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,12 +14,13 @@ echo "lower bound $lower; upper bound $upper"
 
 num_commits=$(awk "BEGIN{srand();print int(rand()*($upper-$lower))+$lower }")
 
-echo git config --global user.name $user_name
-echo git config --global user.email $user_email
+git config --global user.name $user_name
+git config --global user.email $user_email
 
 for c in $(seq 1 $num_commits)
 do
     # Generate empty commits 
-    echo git commit --allow-empty -m "$c"
+    git commit --allow-empty -m "$c"
 done
-    
+
+git push
